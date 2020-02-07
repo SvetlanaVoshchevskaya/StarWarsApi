5877fcf40556b8fdff022936dcabab6a22471db0
Svetlana <VSveta.83@gmail.com>
2020-02-07 21:13:29 +0200

    Updates

---
+++ precache-manifest.4efb2d048ff4c8a1b8d045cedce4cd4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb90fb183b1937537b6f27b06b299546",
+    "url": "/StarWarsApi/index.html"
+  },
+  {
+    "revision": "2498dd64f24c40f808d7",
+    "url": "/StarWarsApi/static/css/main.d09e7eec.chunk.css"
+  },
+  {
+    "revision": "16e67a10d44e9cb58d11",
+    "url": "/StarWarsApi/static/js/2.174868a8.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/StarWarsApi/static/js/2.174868a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2498dd64f24c40f808d7",
+    "url": "/StarWarsApi/static/js/main.bec125b3.chunk.js"
+  },
+  {
+    "revision": "51e1aeb74f923d57ebe0",
+    "url": "/StarWarsApi/static/js/runtime-main.e57b2d35.js"
+  }
+]);
